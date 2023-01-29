6ac92c899c8d896f735e596d884451934252c685
viktor111 <swifrorlilko@gmail.com>
2023-01-29 09:30:40 +0200

    List looping over

--- basics/README.md
+++ basics/README.md
@@ -2,20 +2,8 @@
 
 **TODO: Add description**
 
-## Installation
-
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `basics` to your list of dependencies in `mix.exs`:
-
-```elixir
-def deps do
-  [
-    {:basics, "~> 0.1.0"}
-  ]
-end
-```
-
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at <https://hexdocs.pm/basics>.
-
+## Run
+`cd Basics`
+`iex -S mix`
+`Basics.call_fn`
+`recompile`
--- basics/lib/basics.ex
+++ basics/lib/basics.ex
@@ -1,8 +1,25 @@
 defmodule Basics do
+  names = ["Heisenberg", "Potter", "Buddy"]
+
   def add_two_numbers_piping() do
     num1 = IO.gets("Enter a number: ") |> String.trim |> String.to_integer
     num2 = IO.gets("Enter another number: ") |> String.trim |> String.to_integer
 
     IO.puts("The sum of the two numbers is #{num1 + num2}")
+  end
+
+  def for_loop_over_list() do
+    names = ["Heisenberg", "Potter", "Buddy"]
+
+    for name <- names do
+      IO.puts(name)
+    end
+  end
+
+  def each_loop_over_list() do
+    names = ["Heisenberg", "Potter", "Buddy"]
+    Enum.each(names, fn(name) ->
+      IO.puts(name)
+    end)
   end
 end
