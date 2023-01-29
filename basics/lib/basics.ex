0dfb40eed0c0db1c507bb6e3ee4a79e202d56591
viktor111 <swifrorlilko@gmail.com>
2023-01-29 12:56:52 +0200

    add requests post get

--- basics/lib/basics.ex
+++ basics/lib/basics.ex
@@ -1,7 +1,7 @@
 defmodule Basics do
   def add_two_numbers_piping() do
-    num1 = IO.gets("Enter a number: ") |> String.trim |> String.to_integer
-    num2 = IO.gets("Enter another number: ") |> String.trim |> String.to_integer
+    num1 = IO.gets("Enter a number: ") |> String.trim() |> String.to_integer()
+    num2 = IO.gets("Enter another number: ") |> String.trim() |> String.to_integer()
 
     IO.puts("The sum of the two numbers is #{num1 + num2}")
   end
@@ -17,7 +17,7 @@ defmodule Basics do
   def each_loop_over_list() do
     names = ["Heisenberg", "Potter", "Buddy"]
 
-    Enum.each(names, fn(name) ->
+    Enum.each(names, fn name ->
       IO.puts(name)
     end)
   end
@@ -37,4 +37,46 @@ defmodule Basics do
     # when index is out of range
   end
 
+  def send_http_get() do
+    :inets.start()
+    :ssl.start()
+
+    url = 'https://jsonplaceholder.typicode.com/todos/'
+    headers = [{'accept', 'application/json'}]
+
+    http_request_opts = [
+      ssl: [
+        verify: :verify_peer,
+        cacerts: :public_key.cacerts_get(),
+        customize_hostname_check: [
+          match_fun: :public_key.pkix_verify_hostname_match_fun(:https)
+        ]
+      ]
+    ]
+
+    :httpc.request(:get, {url, headers}, http_request_opts, [])
+
+  end
+
+  def send_http_post() do
+    :inets.start()
+    :ssl.start()
+
+    url = 'https://httpbin.org/post'
+    headers = []
+    content_type = ''
+    body = ''
+
+    http_request_opts = [
+      ssl: [
+        verify: :verify_peer,
+        cacerts: :public_key.cacerts_get(),
+        customize_hostname_check: [
+          match_fun: :public_key.pkix_verify_hostname_match_fun(:https)
+        ]
+      ]
+    ]
+
+    :httpc.request(:post, {url, headers, content_type, body}, http_request_opts, [])
+  end
 end
